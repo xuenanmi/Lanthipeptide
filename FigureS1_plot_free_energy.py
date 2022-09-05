e5686d05217534ce6b2a878f1b8917ef8a307eb1
xuenanmi <80510335+xuenanmi@users.noreply.github.com>
2022-09-04 20:30:42 -0500

    Update and rename plot_free_energy.py to FigureS1_plot_free_energy.py

--- FigureS1_plot_free_energy.py
+++ FigureS1_plot_free_energy.py
@@ -3,17 +3,17 @@ import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
-
-import argparse
 import glob
+import pickle
 plt.rc('savefig', dpi=500)
 matplotlib.rc('font',family='Helvetica-Normal',size=13)
 
 name = 'procA3.3WT_cluster_150_ticdim_10'
 data_name='procA3.3WT-four-dist-sub-helical'
 
+#define a function to get the probabilty of each 2-D bin
+#four parameters: x-axis, y-axis, the number of bins, weights of frame
 def get_prob_density(data1, data2, bins=100, weights=None):
-
     if len(np.shape(data1)) > 1:
         data1 = np.asarray(data1)[:,0]
         data2 = np.asarray(data2)[:,0]
@@ -26,6 +26,8 @@ def get_prob_density(data1, data2, bins=100, weights=None):
 
     return prob_density.T, x_coords, y_coords
 
+#define a function to plot 2-D free energy landscape
+#ten parameters: x-axis, y-axis, temperture, weigts of frame, the number of bins, weights of frame, limits of x-axis and y-axis, max energy, x-label, y-label, figure name, figure title
 def free_energy(data1, data2, T=300, weights=None, lims = None, max_energy=5, label1="Label Your Axes!", label2="Label Your Axes!", savename="fe.png", title = None):
 
     #compute free energy
@@ -42,27 +44,22 @@ def free_energy(data1, data2, T=300, weights=None, lims = None, max_energy=5, la
     cbar.set_label("Free Energy (kcal/mol)",size=16)
     cbar.ax.set_yticklabels(range(max_energy+1))
     cbar.ax.tick_params(labelsize=16)
-    
-    #plt.contour(X,Y,free_energy, linewidths=0.25, levels=[0,1,2,3,4,5], colors='black')
- 
+   
     plt.tick_params(axis='both',labelsize=12)
     plt.xlabel(label1)
     plt.ylabel(label2)
-    #plt.ylim(1,9)
-    #plt.xlim(0,1)
     plt.xlim(lims[0],lims[1])
     plt.ylim(lims[2],lims[3])
     plt.title(title)
-    #axes = plt.axes()
     
-    
+    #set xticks
     if lims[1] - lims[0] <= 1:
         ax.xaxis.set_major_locator(plt.MultipleLocator(0.1))
     elif lims[1] - lims[0] <= 2:
         ax.xaxis.set_major_locator(plt.MultipleLocator(0.2))
     else:
         ax.xaxis.set_major_locator(plt.MultipleLocator(1.0))
-
+    #set yticks
     if lims[3] - lims[2] <= 1:
         ax.yaxis.set_major_locator(plt.MultipleLocator(0.2))
     elif lims[3] - lims[2] <= 2:
@@ -71,37 +68,27 @@ def free_energy(data1, data2, T=300, weights=None, lims = None, max_energy=5, la
         ax.yaxis.set_major_locator(plt.MultipleLocator(5.0))
     
     plt.grid(linestyle=":")
-    #ax.spines['bottom'].set_linewidth(1)
-    #ax.spines['left'].set_linewidth(1)
-    #ax.spines['top'].set_linewidth(1)
-    #ax.spines['right'].set_linewidth(1)
-    #plt.gca().set_aspect(aspect=1.0, adjustable='box')
     fig.tight_layout()
-
-    #plt.plot(4.9,2.7, 'ko', markersize=4, mec='w', mew=0.3)
-    plt.plot(0.33,5.7, 'm*', markersize=16, mec='w', mew=0.3)
-
-
+    plt.plot(0.33,5.7, 'm*', markersize=16, mec='w', mew=0.3) #annotate minima in the free energy landscape
     plt.savefig(savename, transparent=False)
 
 
 if __name__=="__main__":
 
-
     ax1_data = []
     ax2_data = []
-    import pickle
-    data = pickle.load(open(data_name +'.pkl', 'rb'), encoding='latin1')
+
+    data = pickle.load(open(data_name +'.pkl', 'rb'), encoding='latin1') #load data
     for i in range(len(data)):
         for j in range(len(data[i])):
-             ax1_data.append(data[i][j][4])
-             ax2_data.append(data[i][j][1]*10)
+             ax1_data.append(data[i][j][4]) #for FigureS1, x-axis is helical content
+             ax2_data.append(data[i][j][1]*10) #for FigureS1, y-axis is distance of T11 and C14
     
     print(np.shape(ax1_data))
     print(np.shape(ax2_data))
 
     
-    weights_all = pickle.load(open('MSM-'+name+'-weights.pkl','rb'), encoding='latin1')
+    weights_all = pickle.load(open('MSM-'+name+'-weights.pkl','rb'), encoding='latin1') #load weights of frame
     weights = []
     for traj in weights_all:
         weights.extend(traj)
